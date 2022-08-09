4620f3f84da6b177011744133b4776aa0f332400
Adam West <Adamwest023@gmail.com>
2022-08-09 14:57:01 -0500

    added animation and array methods to update arrays

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+//set these for the timestamp 
+let timeToNextRaven = 0;
+let ravenInterval = 500;
+let lastTime = 0;
+
 let ravens = []
+
 class Raven {
     constructor() {
         this.width = 100;
@@ -13,22 +19,35 @@ class Raven {
         this.y = Math.random() * (canvas.height - this.height);
         this.directionX = Math.random() * 5 + 3;
         this.directionY = Math.random() * 5 - 2.5;
+        this.markedForDeletion = false;
     }
     update() {
         this.x -= this.directionX;
+        if(this.x < 0 - this.width) this.markedForDeletion= true;
     }
     draw() {
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 };
 
-const raven = new Raven();
-
+//timestamp is in milliseconds 
 function animate(timestamp) {  
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    raven.update();
-    raven.draw();
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp; 
+    timeToNextRaven += deltaTime;
+    //creates a new raven when the interval is reached.
+    if(timeToNextRaven > ravenInterval){
+        ravens.push(new Raven());
+        timeToNextRaven = 0;
+    };
+    //uses spread operator to add each raven's update and draw methods to the animate function
+    [...ravens].forEach(object => object.update());
+    [...ravens].forEach(object => object.draw());
+    //creates an array with the same name that has removed any objects that are not marked for deletion
+    ravens = ravens.filter(object => !object.markedForDeletion);
+    
     requestAnimationFrame(animate);
 }
 
-animate();
+animate(0);
